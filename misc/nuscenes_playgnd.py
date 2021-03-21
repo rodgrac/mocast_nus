f53ad39d17db882cde14dbd56bc499e3c1b95470
rodgrac <rodneygracian@gmail.com>
2021-03-21 14:14:19 -0400

    Nusc plgd lane info apis

--- misc/nuscenes_playgnd.py
+++ misc/nuscenes_playgnd.py
@@ -29,13 +29,19 @@ instance_token, sample_token = train_set[100].split("_")
 
 annot1 = helper.get_sample_annotation(instance_token, sample_token)
 
-future_xy = helper.get_annotations_for_sample(sample_token)
+map_name = helper.get_map_name_from_sample_token(sample_token)
 
-nusc_map = NuScenesMap(map_name='singapore-onenorth', dataroot=NUSCENES_DATASET)
+nusc_map = NuScenesMap(map_name=map_name, dataroot=NUSCENES_DATASET)
 
-x, y, yaw = 395, 1095, 0
+x, y, yaw = 1281, 1033, 0
 closest_lane = nusc_map.get_closest_lane(x, y, radius=2)
-# print(arcline_path_utils.discretize_lane(nusc_map.get_arcline_path(closest_lane), resolution_meters=1))
+print(closest_lane)
+path = nusc_map.get_arcline_path(closest_lane)
+print(nusc_map.get_incoming_lane_ids(closest_lane))
+print(nusc_map.get_outgoing_lane_ids(closest_lane))
+print(arcline_path_utils.project_pose_to_lane((x, y, yaw), path))
+print(nusc_map.drivable_area)
+#print(arcline_path_utils.discretize_lane(nusc_map.get_arcline_path(closest_lane), resolution_meters=1))
 
 static_rast = StaticLayerRasterizer(helper)
 agent_rast = AgentBoxesWithFadedHistory(helper, 3)
@@ -43,13 +49,13 @@ input_rep = InputRepresentation(static_rast, agent_rast, Rasterizer())
 
 img = input_rep.make_input_representation(instance_token, sample_token)
 
-print([helper.get_velocity_for_agent(instance_token, sample_token),
-                                    helper.get_acceleration_for_agent(instance_token, sample_token),
-                                    helper.get_heading_change_rate_for_agent(instance_token, sample_token)])
+# print([helper.get_velocity_for_agent(instance_token, sample_token),
+#                                     helper.get_acceleration_for_agent(instance_token, sample_token),
+#                                     helper.get_heading_change_rate_for_agent(instance_token, sample_token)])
 
-plt.imshow(img)
-
-plt.show()
+# plt.imshow(img)
+#
+# plt.show()
 
 
 
